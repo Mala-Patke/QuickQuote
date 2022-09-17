325e41e31d4f1dab142dad66201b847d25d9a705
Mala Patke <alis@khanlabschool.org>
2022-09-16 17:07:32 -0700

    Add functionality to extension

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,31 @@
-document.getElementById('submit').addEventListener('click', () => {
-    let input = document.getElementById('input').value;
+function addToQuotebook() {
+    let quote = document.getElementById('input');
+    let name = document.getElementById('name');
 
-    fetch('')
-});
+    let params = new URLSearchParams({ quote:`"${quote.value}," - ${name.value}, ${new Date().getFullYear()}` });
+
+    fetch(`https:/quick-quote.vercel.app/api/quote?` + params.toString())
+        .then(res => res.json())
+        .then(data => {
+            if(!`${data.status}`.startsWith('2')) {
+                alert('ERROR! Please try again. If this persists, contact the developer');
+                console.log(data); 
+            }
+
+            quote.value = '';
+            name.value = '';
+
+            let button = document.getElementById('submit');
+            button.disabled = true;
+            setTimeout(() => {
+                button.disabled = false;
+            }, 10000);
+
+            alert('Quote submitted!');
+        });
+}
+
+document.getElementById('submit').addEventListener('click', addToQuotebook);
+document.getElementById('input').addEventListener('keyup', e => 
+    ['enter', 'return'].includes(e.key.toLowerCase()) ? addToQuotebook() : 1
+);
